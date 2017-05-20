423177bbf11ac034c8f3740ba1866f8f38e40fea
ivarus <ivarbjorling@hotmail.com>
2017-05-20 13:01:53 +0200

    Updated Vulkan

--- VulkanProject/StorageBuffer.cpp
+++ VulkanProject/StorageBuffer.cpp
@@ -9,19 +9,19 @@ StorageBuffer::StorageBuffer(VkDevice device, VkPhysicalDevice physicalDevice, u
     mStride = stride;
     
     // Storage buffer.
-    uint32_t minOffsetAligment;
+    uint32_t minOffsetAlignment;
     vkTools::CreateBuffer(mDevice, mPhysicalDevice, mSize,
         VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_DEVICE_LOCAL_BIT,
-        mBuffer, mBufferMemory, minOffsetAligment
+        mBuffer, mBufferMemory, minOffsetAlignment
         );
-    MsgAssert(mStride % minOffsetAligment, 0, "Vulkan runtime error. VKERROR: Unsupported storage buffer aligment.");
+    MsgAssert(mStride % minOffsetAlignment, 0, "Vulkan runtime error. VKERROR: Unsupported storage buffer alignment.");
 
     // Staging buffer.
     vkTools::CreateBuffer(mDevice, mPhysicalDevice, mSize,
         VK_BUFFER_USAGE_STORAGE_BUFFER_BIT | VK_BUFFER_USAGE_TRANSFER_SRC_BIT | VK_BUFFER_USAGE_TRANSFER_DST_BIT, VK_MEMORY_PROPERTY_HOST_VISIBLE_BIT | VK_MEMORY_PROPERTY_HOST_COHERENT_BIT,
-        mStagingBuffer, mStagingBufferMemory, minOffsetAligment
+        mStagingBuffer, mStagingBufferMemory, minOffsetAlignment
         );
-    MsgAssert(mStride % minOffsetAligment, 0, "Vulkan runtime error. VKERROR: Unsupported storage buffer aligment.");
+    MsgAssert(mStride % minOffsetAlignment, 0, "Vulkan runtime error. VKERROR: Unsupported storage buffer alignment.");
 }
 
 StorageBuffer::~StorageBuffer()
